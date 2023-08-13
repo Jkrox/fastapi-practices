128c9fc5b59f6e8b08021eb2713612e6bc89140b
Jkrox <jkrox@protonmail.com>
2023-08-13 17:20:56 -0500

    feat(movie): Adding new folder for the routers.

---
+++ routers/movie.py
@@ -0,0 +1,176 @@
+from fastapi import APIRouter
+from fastapi import Depends, Path, Query, HTTPException, status, Request
+from fastapi.responses import JSONResponse
+from fastapi.security.http import HTTPBearer, HTTPAuthorizationCredentials
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from config.database import base, engine, session
+from middlewares import jwt_bearer
+from models.movie import Movie as MovieModel
+from fastapi.encoders import jsonable_encoder
+
+import datetime
+movie_router = APIRouter()
+
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(..., min_length=1, max_length=25)
+    overview: str = Field(min_length=15, max_length=150)
+    year: int = Field(ge=2000, le=datetime.datetime.now().year)
+    rating: float = Field(ge=1, le=10)
+    category: str = Field(..., min_length=3, max_length=15)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "Película: ",
+                "overview": "Descripción de la película: .",
+                "year": 2004,
+                "rating": 9.3,
+                "category": "Drama",
+            },
+            "description": "This is an example of a movie object that can be used in the API.",
+            "externalDocs": {
+                "description": "More information about movies",
+                "url": "https://en.wikipedia.org/wiki/List_of_films_considered_the_best",
+            },
+        }
+
+
+# --------------------------------------------------------------------------------
+
+
+@movie_router.get(
+    "/movies",
+    tags=["movies"],
+    response_model=List[Movie],
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(jwt_bearer())],
+)
+def get_movies() -> List[Movie]:
+    db = session()
+    result = db.query(MovieModel).all()
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+
+
+# --------------------------------------------------------------------------------
+
+
+@movie_router.get(
+    "/movies/{id}",
+    tags=["movies"],
+    response_model=Movie,
+    status_code=status.HTTP_200_OK,
+)
+def get_movie(id: int = Path(ge=1, le=200)) -> Movie:
+    db = session  # The handler_error middleware must appear here.
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        raise HTTPException(status_code=404, detail="Movie not found.")
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+
+
+# --------------------------------------------------------------------------------
+
+
+@movie_router.get(
+    "/movies/",
+    tags=["movies"],
+    response_model=List[Movie],
+    status_code=status.HTTP_200_OK,
+)
+def get_movies_by_category(
+    category: str = Query(None, min_length=3, max_length=15)
+) -> List[Movie]:
+    db = session()
+    result = db.query(MovieModel).filter(MovieModel.category == category).all()
+    if not result:
+        raise HTTPException(status_code=404, detail="Movie not found.")
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+
+
+# --------------------------------------------------------------------------------
+
+
+@movie_router.post(
+    "/movies", tags=["movies"], response_model=dict, status_code=status.HTTP_201_CREATED
+)
+def create_movie(movie: Movie) -> dict:
+    db = session()
+    new_movie = MovieModel(**movie.dict())
+    db.add(new_movie)
+    db.commit()
+    return JSONResponse(content={"message": "Movie created"})
+
+
+# --------------------------------------------------------------------------------
+
+
+@movie_router.put(
+    "/movies/{id}", tags=["movies"], response_model=dict, status_code=status.HTTP_200_OK
+)
+def update_movie(id: int, movie: Movie) -> dict:
+    db = session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        raise HTTPException(status_code=404, detail="Movie not found.")
+    result.title = movie.title
+    result.overview = movie.overview
+    result.year = movie.year
+    result.rating = movie.rating
+    result.category = movie.category
+    db.commit()
+    return JSONResponse(status_code=200, content={"message": "Movie modified."})
+
+
+# --------------------------------------------------------------------------------
+
+
+@movie_router.put(
+    "/movies/",
+    tags=["movies"],
+    response_model=List[Movie],
+    status_code=status.HTTP_200_OK,
+)
+def update_movies(ids: List[int], movie: Movie) -> List[Movie]:
+    db = session()
+    updated_movies = []
+    found = False
+
+    for movie_id in ids:
+        result = db.query(MovieModel).filter(MovieModel.id == movie_id).first()
+        if result:
+            found = True
+            result.title = movie.title
+            result.overview = movie.overview
+            result.year = movie.year
+            result.rating = movie.rating
+            result.category = movie.category
+            updated_movies.append(result)
+        else:
+            raise HTTPException(
+                status_code=404, detail="Movie with ID {movie_id} not found.}"
+            )
+
+    if not found:
+        raise HTTPException(status_code=404, detail="Movie not found.")
+
+    return JSONResponse(status_code=200, content=jsonable_encoder(updated_movies))
+
+
+# --------------------------------------------------------------------------------
+
+
+@movie_router.delete(
+    "/movies/{id}", tags=["movies"], response_model=dict, status_code=status.HTTP_200_OK
+)
+def delete_movie(id: int) -> dict:
+    db = session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        raise HTTPException(status_code=404, detail="Movie not found.")
+
+    db.delete(result)
+    db.commit()
+    return JSONResponse(content={"message": "Movie deleted."})
